93c50a27a0bafa93c59684b914ff1d9214203288
zenoftrading <zenoftrading@yanhoo.com>
2021-03-02 22:42:06 +0300

    Update barackbot.py

--- barackbot.py
+++ barackbot.py
@@ -36,15 +36,16 @@ def on_the_opening_candle():
     Returns:
         float: purchase price
     """
+    global free_usdt
     try:
         last_two_candles = client.get_klines(symbol='BNBUSDT', interval=Client.KLINE_INTERVAL_1MINUTE, limit=2)
         range_prev_day = float(last_two_candles[-2][2])-float(last_two_candles[-2][3])
-        entry_price = float(last_two_candles[-1][1])+range_prev_day
-        free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
+        entry_price = float(last_two_candles[-1][1])+range_prev_day*0.5
+        new_free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
         free_bnb = float(client.get_asset_balance(asset='BNB')['free'])
-        variables['free_usdt'] = free_usdt
-        print("Free USDT is {}, free BNB is {}".format(free_usdt,free_bnb))
-        print("Entry price is {} USDT, quantity is {} BNB".format(entry_price,variables['quantity']))
+        free_usdt = new_free_usdt
+        print("Free USDT is {}, free BNB is {}".format(new_free_usdt,free_bnb))
+        print("Entry price is {} USDT, quantity is {} BNB".format(entry_price,quantity))
 
         return entry_price
     except Exception as e:
@@ -98,15 +99,16 @@ def take(quantity, open_price):
     Returns:
         boolean: True if take order is finished and False if error
     """
+    global free_usdt
     try:
         print("Take order. Open price is {} USDT".format(open_price))
         order = client.order_market_sell(
             symbol='BNBUSDT',
             quantity=quantity
         )
-        free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
-        print("Profit is {} USDT".format(variables['free_usdt']-free_usdt))
-        variables['free_usdt'] = free_usdt
+        new_free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
+        print("Profit is {} USDT".format(free_usdt-new_free_usdt))
+        free_usdt = new_free_usdt
         print("Take order info: {}".format(order))
         return True
     except Exception as e:
@@ -123,15 +125,16 @@ def stop(quantity, open_price):
     Returns:
         boolean: True if stop order is finished and False if error
     """
+    global free_usdt
     try:
         print("Stop order. Open price is {} USDT".format(open_price))
         order = client.order_market_sell(
             symbol='BNBUSDT',
             quantity=quantity
         )
-        free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
-        print("Loss is {} USDT".format(free_usdt-variables['free_usdt']))
-        variables['free_usdt'] = free_usdt
+        new_free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
+        print("Loss is {} USDT".format(new_free_usdt-free_usdt))
+        free_usdt = new_free_usdt
         print("Stop order info: {}".format(order))
         return True
     except Exception as e:
@@ -145,28 +148,29 @@ def on_message(ws, msg):
         ws: websocket
         msg: websocket message
     """
+    global is_candle_opened, in_position, entry_price, quantity, free_usdt
     try:
         json_message = json.loads(msg)
         candle = json_message['k']
         is_candle_closed = candle['x']
-        if variables['is_candle_opened']:
-            if variables['in_position']:
+        if is_candle_opened:
+            if in_position:
                 # stop or take
-                if float(candle['o']) > variables['entry_price']:
-                    take(variables['quantity'], candle['o'])
-                    variables['in_position'] = False
-                elif float(candle['o']) <= variables['entry_price']:
-                    stop(variables['quantity'], candle['o'])
-                    variables['in_position'] = False
-            variables['entry_price'] = on_the_opening_candle()
-            variables['is_candle_opened'] = False
+                if float(candle['o']) > entry_price:
+                    take(quantity, candle['o'])
+                    in_position = False
+                elif float(candle['o']) <= entry_price:
+                    stop(quantity, candle['o'])
+                    in_position = False
+            entry_price = on_the_opening_candle()
+            is_candle_opened = False
 
         # check entry price
-        if not variables['in_position'] and float(candle['h']) >= variables['entry_price']:
-            variables['in_position'] = buy(variables['entry_price'], variables['quantity'])
+        if not in_position and float(candle['h']) >= entry_price:
+            in_position = buy(entry_price, quantity)
 
         if is_candle_closed:
-            variables['is_candle_opened'] = True
+            is_candle_opened = True
 
         print_current_candle(candle)
     except Exception as e:
@@ -181,13 +185,11 @@ client.API_URL = 'https://testnet.binance.vision/api'
 # client = Client(keys.BINANCE_API, keys.BINANCE_SECRET)
 socket = "wss://stream.binance.com:9443/ws/bnbusdt@kline_1m"
 
-variables = {
-    'is_candle_opened': True,
-    'in_position': False,
-    'entry_price': 9999999.,
-    'quantity': 0.1,
-    'free_usdt': 0
-}
+is_candle_opened = True
+in_position = False
+entry_price = 9999999.
+quantity = 0.1
+free_usdt = 0
 
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
