1757351cced5052b23a74aa5400969efa550e368
zenoftrading <zenoftrading@yahoo.com>
2021-03-03 11:20:55 +0300

    Update barackbot.py
    v0.1

--- barackbot.py
+++ barackbot.py
@@ -2,7 +2,27 @@ import websocket, json
 from binance.client import Client
 from binance.enums import KLINE_INTERVAL_1MINUTE
 from datetime import datetime
-import keys
+
+TEST_BINANCE_API = ''
+TEST_BINANCE_SECRET = ''
+
+BINANCE_API = ''
+BINANCE_SECRET = ''
+
+# for testnet trading
+test_client = Client(TEST_BINANCE_API, TEST_BINANCE_SECRET)
+test_client.API_URL = 'https://testnet.binance.vision/api'
+# test_socket = "wss://testnet.binance.vision/ws/bnbusdt@kline_1m"
+
+# for production and real prices
+client = Client(BINANCE_API, BINANCE_SECRET)
+socket = "wss://stream.binance.com:9443/ws/bnbusdt@kline_1m"
+
+is_candle_opened = True
+in_position = False
+entry_price = 9999999.
+quantity = 0.1
+free_usdt = 0
 
 def on_open(ws):
     """Do something when websocket opening.
@@ -36,13 +56,13 @@ def on_the_opening_candle():
     Returns:
         float: purchase price
     """
-    global free_usdt
+    global free_usdt, client, test_client
     try:
         last_two_candles = client.get_klines(symbol='BNBUSDT', interval=Client.KLINE_INTERVAL_1MINUTE, limit=2)
         range_prev_day = float(last_two_candles[-2][2])-float(last_two_candles[-2][3])
         entry_price = float(last_two_candles[-1][1])+range_prev_day*0.5
-        new_free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
-        free_bnb = float(client.get_asset_balance(asset='BNB')['free'])
+        new_free_usdt = float(test_client.get_asset_balance(asset='USDT')['free'])
+        free_bnb = float(test_client.get_asset_balance(asset='BNB')['free'])
         free_usdt = new_free_usdt
         print("Free USDT is {}, free BNB is {}".format(new_free_usdt,free_bnb))
         print("Entry price is {} USDT, quantity is {} BNB".format(entry_price,quantity))
@@ -75,10 +95,11 @@ def buy(entry_price, quantity):
     Returns:
         boolean: True if buying order is finished and False if error
     """
+    global test_client
     try:
         precision_entry_price = precision_price(entry_price)
         print("Buying {} BNB at {} USDT".format(quantity,precision_entry_price))
-        order = client.order_limit_buy(
+        order = test_client.order_limit_buy(
             symbol='BNBUSDT',
             quantity=quantity,
             price=precision_entry_price
@@ -99,14 +120,14 @@ def take(quantity, open_price):
     Returns:
         boolean: True if take order is finished and False if error
     """
-    global free_usdt
+    global free_usdt, test_client
     try:
         print("Take order. Open price is {} USDT".format(open_price))
-        order = client.order_market_sell(
+        order = test_client.order_market_sell(
             symbol='BNBUSDT',
             quantity=quantity
         )
-        new_free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
+        new_free_usdt = float(test_client.get_asset_balance(asset='USDT')['free'])
         print("Profit is {} USDT".format(free_usdt-new_free_usdt))
         free_usdt = new_free_usdt
         print("Take order info: {}".format(order))
@@ -125,14 +146,14 @@ def stop(quantity, open_price):
     Returns:
         boolean: True if stop order is finished and False if error
     """
-    global free_usdt
+    global free_usdt, test_client
     try:
         print("Stop order. Open price is {} USDT".format(open_price))
-        order = client.order_market_sell(
+        order = test_client.order_market_sell(
             symbol='BNBUSDT',
             quantity=quantity
         )
-        new_free_usdt = float(client.get_asset_balance(asset='USDT')['free'])
+        new_free_usdt = float(test_client.get_asset_balance(asset='USDT')['free'])
         print("Loss is {} USDT".format(new_free_usdt-free_usdt))
         free_usdt = new_free_usdt
         print("Stop order info: {}".format(order))
@@ -148,7 +169,7 @@ def on_message(ws, msg):
         ws: websocket
         msg: websocket message
     """
-    global is_candle_opened, in_position, entry_price, quantity, free_usdt
+    global is_candle_opened, in_position, entry_price, quantity
     try:
         json_message = json.loads(msg)
         candle = json_message['k']
@@ -175,21 +196,6 @@ def on_message(ws, msg):
         print_current_candle(candle)
     except Exception as e:
         print("Websocket stream error: {}".format(e))
-
-# for testnet
-client = Client(keys.TEST_BINANCE_API, keys.TEST_BINANCE_SECRET)
-client.API_URL = 'https://testnet.binance.vision/api'
-# socket = "wss://testnet.binance.vision/ws/bnbusdt@kline_1m"
-
-# for production
-# client = Client(keys.BINANCE_API, keys.BINANCE_SECRET)
-socket = "wss://stream.binance.com:9443/ws/bnbusdt@kline_1m"
-
-is_candle_opened = True
-in_position = False
-entry_price = 9999999.
-quantity = 0.1
-free_usdt = 0
 
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
